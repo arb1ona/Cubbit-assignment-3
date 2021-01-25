fe34b1643eda3fb961442ca27c7c2338baa25b72
arb1ona <arbiona.shahu@gmail.com>
2021-01-25 01:16:26 +0100

    TypeScript App setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-family: 'Nunito', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  display: flex;
+`;
+
+export default function App() {
+  return <Container>
+
+    Arbionaaa
+   
+  </Container>
+}
---
+++ src/CheckoutPage.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function CheckoutPage({}: Props): ReactElement {
+    return (
+        <div>
+            asenfanefa
+        </div>
+    )
+}
