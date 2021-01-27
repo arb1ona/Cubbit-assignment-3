b27535d6ab3dcfa6d8b4386ebe9a5843edf65cbd
arb1ona <arbiona.shahu@gmail.com>
2021-01-27 20:31:52 +0100

    Update StyleDiv

--- src/components/StyleDiv.tsx
+++ src/components/StyleDiv.tsx
@@ -119,3 +119,84 @@ padding: 20px;
 display: flex;
 place-content: space-between;
 `;
+
+
+//---------------------------- ORDER INFO SECTION  --------------------------------------------------
+
+export const OrderInfoWrapper = styled.div`
+  h3 { font-weight: 800 }
+
+  @media (min-width: 1170px) {
+    margin: 0 50px;
+    width: 350px;
+  }
+`;
+
+export const CartWrapper = styled.div`
+  border: 1px solid #FFFFFF;
+  background: #FFFFFF;
+  box-sizing: border-box;
+  border-radius: 5px;
+  text-align: center;
+`;
+
+export const Title = styled.div` 
+    font-weight: 800;
+    color: #009EFF;
+    `;
+export const CountButton = styled.button`
+  background: #F7F7F7;
+  border: 1px solid #ECECEC;
+  box-sizing: border-box;
+  border-radius: 5px;
+  font-size: 1rem;
+  cursor: pointer;
+`;
+
+export const CountInput = styled.input`
+  width: 30px;
+  color: #161616;
+  text-align: center;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  background: #FFFFFF;
+  border: 1px solid #ECECEC;
+  box-sizing: border-box;
+  border-radius: 5px;
+`;
+export const CartItem = styled.img`
+  margin: 30px 0;
+  width: 100px;
+`;
+
+export const Price = styled.div`
+  margin: 10px 20px;
+  display: flex;
+  place-content: flex-start space-between;
+  align-items: flex-start;
+}
+`;
+export const PriceLine = styled.div`
+  border-top: 1px solid #B6B6B6;
+  margin: 0 20px;
+`;
+export const PriceTotal = styled.div`
+  margin: 10px 20px;
+  display: flex;
+  place-content: flex-start space-between;
+  align-items: flex-start;
+
+  div {
+    font-weight: 800;
+    color: #000000;
+  }
+}
+`;
+
+export const Info = styled.div`
+  margin: 20px;
+  color: #B0B0B0;
+`;
+export const InfoTick = styled.img`
+  margin-top: 20px
+`;
